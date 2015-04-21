c0253ba7dc3a94fe1ab604513e9dfc96e299269e
Mohammad Azam <azamsharp@gmail.com>
2015-04-21 13:39:02 -0500

    Initial Commit

---
+++ Baggage Claim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baggage Claim
+//
+//  Created by Mohammad AZam on 4/21/15.
+//  Copyright (c) 2015 AzamSharp Consulting LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
